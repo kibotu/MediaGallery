ceebfe337c3760b86ded5763ddf91d5b8ec6ba5e
Jan Rabe <jan.rabe@exozet.com>
2019-09-26 15:45:57 +0200

    init

---
+++ app/src/main/java/net/kibotu/mediagallery/demo/MainActivity.kt
@@ -0,0 +1,23 @@
+package net.kibotu.mediagallery.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // list of media objects
+        // images
+        // streaming urls
+        // click listener
+        // zoomable
+        // player controls
+        // blurry
+
+
+    }
+}
